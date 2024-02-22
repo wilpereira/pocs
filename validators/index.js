adfee247f70158a728dd5d7e5b56a9b275299e0b
William Pereira <william.pereira@math.marketing>
2024-02-22 12:20:33 -0300

    first commit

---
+++ validators/index.js
@@ -0,0 +1,12 @@
+let validators = {
+    itemA: 'test-class-a',
+    itemB: 'test-class-b',
+    itemC: 'test-class-c',
+}
+
+for(let key in validators) {
+    // console.log(validators[key])
+    // let selector = document.querySelector(validators[key])
+    // console.log(element[key] = selector)
+    console.log(typeof validators[key])
+}
